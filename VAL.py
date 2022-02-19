8b87c3d357df4177230ceb6ed6c98355b78892f9
L0L1P0P1 <behradbadeli@gmail.com>
2022-02-19 19:41:48 +0330

    Made the whole thing a single class

--- VAL.py
+++ VAL.py
@@ -1,20 +1,130 @@
 import datetime
+import socket
 import pip._vendor.requests as requests
+import re
+from collections import OrderedDict
 from VALauth import VALauth
 from VALstore import VALstore
 
 
 class VALpy:
-    def __init__(self, userid, headers):
+
+    regions = ['eu', 'na', 'ap', 'ko']
+
+    def __init__(self):
         r = requests.get('https://valorant-api.com/v1/version')
-        self.version = r.json()        
-        self.headers = {"X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9",
-                        "X-Riot-ClientVersion": self.version['data']['riotClientVersion']}
-        self.headers['Authorization'] = headers['Authorization']
-        self.headers['X-Riot-Entitlements-JWT'] = headers['X-Riot-Entitlements-JWT']
+        self.version = r.json()['data']
+        self.authenticated = False
+
+    def Authenticate(self, username, password, region):
+        self.username = str(username)
+        self.password = str(password)
+
+        if self.regions.count(region) == 0:
+            print('Invalid Region!')
+            return 0
+      
+        if self.authenticated == False:
+
+            # gets address info with "socket"
+            addrinfo = socket.getaddrinfo('auth.riotgames.com', 443)
+            (family, type, proto, canonname, (address, port)) = addrinfo[0]
+
+            # Headers
+            headers = OrderedDict({
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Host': "auth.riotgames.com",
+                'User-Agent': 'RiotClient/43.0.1.4195386.4190634 rso-auth (Windows;10;;Professional, x64)'
+            })
+
+            session = requests.session()
+            session.headers = headers
+
+            # Auth Cookies
+            data = {
+                'client_id': 'play-valorant-web-prod',
+                'nonce': '1',
+                'redirect_uri': 'https://playvalorant.com/opt_in',
+                'response_type': 'token id_token',
+            }
+
+            r = session.post(f'https://{address}/api/v1/authorization', json=data, headers=headers, verify=False)
+
+            # Auth Request
+            data = {
+                'type': 'auth',
+                'username': self.username,
+                'password': self.password
+            }
+            r = session.put(f'https://{address}/api/v1/authorization', json=data, headers=headers, verify=False)
+
+            # 2 FAC
+            if r.json()['data']['type'] == "multifactor":
+                email = r.json()['data']['multifactor']['email']
+                code = input(f'Please Enter The Code Sent to {email}: ')
+                data = {
+                    "type": "multifactor",
+                    "code": code,
+                    "rememberDevice": False
+                }
+        
+            pattern = re.compile('access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)')
+            data = pattern.findall(r.json()['response']['parameters']['uri'])[0] 
+            access_token = data[0]
+            
+            # Entitlement
+            headers = {
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Host': "entitlements.auth.riotgames.com",
+                'User-Agent': 'RiotClient/43.0.1.4195386.4190634 rso-auth (Windows;10;;Professional, x64)',
+                'Authorization': f'Bearer {access_token}',
+            }
+            r = session.post('https://entitlements.auth.riotgames.com/api/token/v1', headers=headers, json={})
+            entitlements_token = r.json()['entitlements_token']
+            # print('Entitlements Token: ' + entitlements_token)
+
+            # Player Info
+            headers = {
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Host': "auth.riotgames.com",
+                'User-Agent': 'RiotClient/43.0.1.4195386.4190634 rso-auth (Windows;10;;Professional, x64)',
+                'Authorization': f'Bearer {access_token}',
+            }
+
+            r = session.post('https://auth.riotgames.com/userinfo', headers=headers, json={})
+            userid = r.json()['sub']
+            # print('User ID: ' + user_id)
+            headers['X-Riot-Entitlements-JWT'] = entitlements_token
+            del headers['Host']
+            session.close()
+            self.userid = userid
+            self.headers = headers
+            self.authenticated = True
+        else:
+            print('Authenticated already! Please create a new Object!')
 
     def GetVersion(self):
         return self.version
+
+    def GetStore(self):
+        if self.authenticated == False:
+            print('Not Authenticated!, Use <classname>.Authenticate(<username>, <password>, <region>) first!')
+            return 0
+
+        r = requests.get(f'https://pd.{self.region}.a.pvp.net/store/v2/storefront/{self.userid}', headers=self.headers, verify=False)
+        r = r['data']
+        return r.text
+
+    def GetStoreJSON(self):
+        if self.authenticated == False:
+            print('Not Authenticated!, Use <classname>.Authenticate(<username>, <password>, <region>) first!')
+            return 0
+
+        r = requests.get(f'https://pd.{self.region}.a.pvp.net/store/v2/storefront/{self.userid}', headers=self.headers, verify=False)
+        r = r['data']
+        return r.json()
+        
+
 
     def FetchWeapons(self):
         r = requests.get('https://valorant-api.com/v1/weapons',verify=False)
@@ -26,14 +136,5 @@ class VALpy:
 
 
 
-if  __name__ == "__main__":
-
-    ValAuth = VALauth("The0Rand0m", "Kayoun1388$")
-    userid, headers = ValAuth.authenticate()
-#    VAL = VALpy(userid, headers)
-#    VAL.FetchWeapons()
-    valstore = VALstore(userid, headers, 'eu')
-    valstore.GetStore()
-    
 
 
